61b01a139f306d55e4daaf3d9f146a717447d011
unjoo <unjoo31@gmail.com>
2024-06-20 09:37:07 +0900

    first page 수정

---
+++ android/app/src/main/java/com/example/chatgptapitest/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.chatgptapitest;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/FirstPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("궁금한게 뭐야?"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+          ),
+        ],
+      ),
+    );
+  }
+}
